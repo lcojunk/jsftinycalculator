c69552beb898899a1a6c36a4c73926f1b63c6da5
Leon Odzhara-Ongom <leon.odzhara-ongom@adesso.de>
2018-01-15 15:33:36 +0100

    Basic arithmetics

---
+++ src/java/calculator/CalculatorBean.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author odzhara-ongom
+ */
+@ManagedBean
+@RequestScoped
+public class CalculatorBean {
+
+    private int _param1;
+    private int _param2;
+    private String _result;
+
+    /**
+     * Creates a new instance of CalculatorBean
+     */
+    public CalculatorBean() {
+    }
+
+    public int getParam1() {
+        return _param1;
+    }
+
+    public void setParam1(int _param1) {
+        this._param1 = _param1;
+    }
+
+    public int getParam2() {
+        return _param2;
+    }
+
+    public void setParam2(int _param2) {
+        this._param2 = _param2;
+    }
+
+    public String getResult() {
+        return _result;
+    }
+
+    public String add() {
+        _result = Integer.toString(_param1 + _param2);
+        return "";
+    }
+
+    public String substract() {
+        _result = Integer.toString(_param1 - _param2);
+        return "";
+    }
+
+    public String multiply() {
+        _result = Integer.toString(_param1 * _param2);
+        return "";
+    }
+
+    public String divide() {
+        _result = _param2 == 0 ? "n/a" : Double.toString(_param1 / (double) _param2);
+        return "";
+    }
+}
